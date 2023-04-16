00b6d06ca9e3ff61ee3c9db46e79bcf29638e483
Prachi tote <prachi.tote77@gmail.com>
2023-04-16 20:45:09 +0530

    Program to find Consonant or Vowel

--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -8,72 +8,20 @@ using static BasicCorePrograms.VowelorConsonent;
 
 namespace BasicCsharpProgram
 {
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please choose any one program from below options");
-            Console.WriteLine("1.QuotientAndReminder\n2.SwapNumbers\n3.PrimeFactorization\n4.HarmonicNumber\n5.FlipCoin\n6.EvenOdd\n7.VowelConsonent\n8.Leapyear\n9.Powerof2");
-            int option = Convert.ToInt32(Console.ReadLine());
-            switch (option)
-            {
-                case 1:
-                    Console.WriteLine("Enter divisor");
-                    int Divisor = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine("Enter divident");
-                    int Divident = Convert.ToInt32(Console.ReadLine());
-                    QuotientAndReminder.QuotientReminder(Divisor, Divident);
-                    Console.ReadLine();
-                    break;
-
-                case 2:
-                    SwapNumbers.SwapNum();
-                    Console.ReadLine();
-                    break;
-
-                case 3:
-                    Console.WriteLine("Print the prime factors of number n");
-                    int n = Convert.ToInt32(Console.ReadLine());
-                    PrimeFactorization.primeFactors(n);
-                    Console.ReadLine();
-                    break;
-
-                case 4:
-                    HarmonicNumber harmonicNumber = new HarmonicNumber();
-                    harmonicNumber.HarmonicNum();
-                    break;
-
-                case 5:
-                    FlipCoin flip = new FlipCoin();
-                    flip.flipCoin();
-                    break;
-
-                case 6:
-                    Console.WriteLine("Please enter integer number to find even or odd");
-                    int input = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine(EvenorOddNumbers.FindEvenorOdd(input));
-                    Console.ReadLine();
-                    break;
-                case 7:
-                    VowelOrConsonant vowelOrConsonant = new VowelOrConsonant();
-                    vowelOrConsonant.alphabet();
-                    break;
-                case 8:
-                    Leapyear leapyear = new Leapyear();
-                    leapyear.Year();
-                    break;
-                case 9:
-                    Powerof2 powerof = new Powerof2();
-                    powerof.powOf2();
-                    break;
-
-            }
+            Console.WriteLine("Welcome to Basic Core Program");
+            Console.WriteLine("Program to find Consonant or Vowel");
+            VowelOrConsonant vowelOrConsonant = new VowelOrConsonant();
+            vowelOrConsonant.alphabet();
         }
     }
 }
-
-
-
+ 
+                 
 
 
 
