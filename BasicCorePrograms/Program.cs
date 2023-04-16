dd756c3809f9180d3c6a71970a780bf09faa4ce7
Prachi tote <prachi.tote77@gmail.com>
2023-04-16 16:36:37 +0530

    Basic Core Program

--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -13,7 +13,7 @@ namespace BasicCsharpProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Please choose any one program from below options");
-            Console.WriteLine("1.QuotientAndReminder\n2.SwapNumbers\n3.PrimeFactorization\n4.HarmonicNumber\n5.FlipCoin\n6.EvenOdd\n8.Leapyear\n9.Powerof2");
+            Console.WriteLine("1.QuotientAndReminder\n2.SwapNumbers\n3.PrimeFactorization\n4.HarmonicNumber\n5.FlipCoin\n6.EvenOdd\n7.VowelConsonent\n8.Leapyear\n9.Powerof2");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
