0a0efb85a054f676fbf9fb1adec70b606a1793e5
Prachi tote <prachi.tote77@gmail.com>
2023-04-16 16:23:17 +0530

    Basic Core Program

--- BasicCorePrograms/SwapNumbers.cs
+++ BasicCorePrograms/SwapNumbers.cs
@@ -10,12 +10,25 @@ namespace BasicCorePrograms
     {
         public static void SwapNum()
         {
-            int a = 5, b = 10;
-            Console.WriteLine("Before swap a= " + a + " b= " + b);
-            a = a + b; //a=15 (5+10)      
-            b = a - b; //b=5 (15-10)      
-            a = a - b; //a=10 (15-5)   
-            Console.Write("After swap a= " + a + " b= " + b);
+            
+            
+                Console.WriteLine("Enter the two numbers");
+                Console.WriteLine("1st number");
+                int a = int.Parse(Console.ReadLine());
+                Console.WriteLine("2nd number");
+                int b = int.Parse(Console.ReadLine());
+                Console.WriteLine("Before swap a = " + a + "  b = " + b);
+                a = a + b;
+                b = a - b;
+                a = a - b;
+                Console.Write("After swap a = " + a + "  b = " + b);
+            
+            //int a = 5, b = 10;
+            //Console.WriteLine("Before swap a= " + a + " b= " + b);
+            //a = a + b; //a=15 (5+10)      
+            //b = a - b; //b=5 (15-10)    /  
+            //a = a - b; //a=10 (15-5)   
+            //Console.Write("After swap a= " + a + " b= " + b);
         }
     }
 }
