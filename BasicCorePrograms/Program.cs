665ae7342c9ad4d0b6238429443307c3763c389f
Prachi tote <prachi.tote77@gmail.com>
2023-04-16 18:19:08 +0530

    Program for Leap Year

--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -1,10 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-using BasicCorePrograms;
+﻿using BasicCorePrograms;
 using System;
-using System.Diagnostics.Metrics;
-using System.Numerics;
-using System.Security.Cryptography;
-using static BasicCorePrograms.VowelorConsonent;
 
 namespace BasicCsharpProgram
 {
@@ -12,60 +7,10 @@ namespace BasicCsharpProgram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please choose any one program from below options");
-            Console.WriteLine("1.QuotientAndReminder\n2.SwapNumbers\n3.PrimeFactorization\n4.HarmonicNumber\n5.FlipCoin\n8.Leapyear");
-            int option = Convert.ToInt32(Console.ReadLine());
-            switch (option)
-            {
-                case 1:
-
-                    //  QuotientAndReminder.QuotientReminder();
-                    Console.WriteLine("Enter divisor");
-                    int Divisor = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine("Enter divident");
-                    int Divident = Convert.ToInt32(Console.ReadLine());
-                    QuotientAndReminder.QuotientReminder(Divisor, Divident);
-                    Console.ReadLine();
-                    break;
-
-                case 2:
-                    SwapNumbers.SwapNum();
-                    Console.ReadLine();
-                    break;
-
-                case 3:
-                    Console.WriteLine("Print the prime factors of number n");
-                    int n = Convert.ToInt32(Console.ReadLine());
-                    PrimeFactorization.primeFactors(n);
-                    Console.ReadLine();
-                    break;
-
-                case 4:
-                    HarmonicNumber harmonicNumber = new HarmonicNumber();
-                    harmonicNumber.HarmonicNum();
-                    break;
-
-                case 5:
-                    FlipCoin flip = new FlipCoin();
-                    flip.flipCoin();
-                    break;
-
-                case 6:
-                    Console.WriteLine("Please enter integer number to find even or odd");
-                    int input = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine(EvenorOddNumbers.FindEvenorOdd(input));
-                    Console.ReadLine();
-                    break;
-                case 7:
-                    VowelOrConsonant vowelOrConsonant = new VowelOrConsonant();
-                    vowelOrConsonant.alphabet();
-                    break;
-                case 8:
-                    Leapyear leapyear = new Leapyear();
-                    leapyear.Year();
-                    break;
-
-            }
+            Console.WriteLine("Welcome to Basic Core Program");
+            Console.WriteLine("Program of flip Coin");
+            Leapyear leapyear = new Leapyear();
+            leapyear.Year();
         }
     }
 }
